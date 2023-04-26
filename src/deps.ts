7129d2a203284147425f2977d3b14475fed0adce
jjchiw <jjchiw@gmail.com>
2023-04-26 19:28:29 +0200

    Fix add article with tags

--- src/deps.ts
+++ src/deps.ts
@@ -8,3 +8,4 @@ export {
 } from 'https://deno.land/x/nessie@2.0.10/mod.ts';
 export type { Info } from 'https://deno.land/x/nessie@2.0.10/mod.ts';
 export { Model, QueryBuilder } from 'https://deno.land/x/vital@v1.1.1/mod.ts';
+export { CORSService } from 'https://deno.land/x/drash@v2.5.0/src/services/cors/cors.ts';
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
-import { Drash } from "./deps.ts";
-import ArticleCommentResource from "./resources/article_comment_resource.ts";
-import ArticlesResource from "./resources/articles_resource.ts";
-import HomeResource from "./resources/home_resource.ts";
-import ProfilesResource from "./resources/profiles_resource.ts";
-import TagsResource from "./resources/tags_resource.ts";
-import UserResource from "./resources/user_resource.ts";
-import UsersLoginResource from "./resources/users_login_resource.ts";
-import UsersResource from "./resources/users_resource.ts";
-import FilesResource from "./resources/files_resource.ts";
+import { Drash, CORSService } from './deps.ts';
+import ArticleCommentResource from './resources/article_comment_resource.ts';
+import ArticlesResource from './resources/articles_resource.ts';
+import HomeResource from './resources/home_resource.ts';
+import ProfilesResource from './resources/profiles_resource.ts';
+import TagsResource from './resources/tags_resource.ts';
+import UserResource from './resources/user_resource.ts';
+import UsersLoginResource from './resources/users_login_resource.ts';
+import UsersResource from './resources/users_resource.ts';
+import FilesResource from './resources/files_resource.ts';
+
+const cors = new CORSService();
 
 export const server = new Drash.Server({
   resources: [
@@ -21,7 +23,8 @@ export const server = new Drash.Server({
     UsersResource,
     FilesResource,
   ],
-  hostname: "realworld_drash",
+  hostname: 'realworld_drash',
   port: 1667,
-  protocol: "http",
+  protocol: 'http',
+  services: [cors],
 });
